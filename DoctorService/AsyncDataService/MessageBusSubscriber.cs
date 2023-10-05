9f66d2d6168ac0f60c9ddbaf6da3539b423d18ff
NeshGogo <rafaelaguero10@hotmail.com>
2023-10-04 21:01:22 -0400

    MessageBusSubscriber at doctorService

--- DoctorService/AsyncDataService/MessageBusSubscriber.cs
+++ DoctorService/AsyncDataService/MessageBusSubscriber.cs
@@ -1,10 +1,84 @@
-﻿namespace DoctorService.AsyncDataService
+﻿using DoctorService.EventProcessing;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+namespace DoctorService.AsyncDataService
 {
     public class MessageBusSubscriber : BackgroundService
     {
+        private readonly IConfiguration _config;
+        private readonly ILogger<MessageBusSubscriber> _logger;
+        private readonly IEventProcessor _eventProcessor;
+        private IConnection _connection;
+        private IModel _channel;
+        private string _queueName;
+
+        public MessageBusSubscriber(
+            IConfiguration config, 
+            ILogger<MessageBusSubscriber> logger, 
+            IEventProcessor eventProcessor)
+        {
+            _config = config;
+            _logger = logger;
+            _eventProcessor = eventProcessor;
+            InitializeRabbitMQ();
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ModuleHandle, ea) => {
+                _logger.LogInformation("--> Event Received!");
+
+                var body = ea.Body;
+                var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+                _eventProcessor.ProcessEvent(notificationMessage);
+            };
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+                _connection.Close();
+            }
+            base.Dispose();
+        }
+
+        private void InitializeRabbitMQ()
+        {
+            var rabbitmqConfig = _config.GetSection("RabbitMQ");
+            var factory = new ConnectionFactory
+            {
+                HostName = rabbitmqConfig.GetValue<string>("Host"),
+                Port = rabbitmqConfig.GetValue<int>("Port"),
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(
+                exchange: rabbitmqConfig.GetValue<string>("Exchange"),
+                ExchangeType.Fanout);
+            _queueName = _channel.QueueDeclare().QueueName;
+            _channel.QueueBind(
+                queue: _queueName,
+                exchange: rabbitmqConfig.GetValue<string>("Exchange"),
+                routingKey: "");
+            _logger.LogInformation("--> Listenting on the Message Bus...");
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+        {
+            _logger.LogInformation("--> Connection Shutdown...");
         }
     }
 }

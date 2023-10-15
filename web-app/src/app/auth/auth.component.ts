43cfb423e0c7df6a979638b728757342193c7282
NeshGogo <rafaelaguero10@hotmail.com>
2023-10-15 13:17:14 -0400

    Auth Login design

--- web-app/src/app/auth/auth.component.ts
+++ web-app/src/app/auth/auth.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
+  form: FormGroup | undefined;
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
+  }
 
+  private buildForm(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  submit(ev: Event):void {
+    ev.preventDefault();
+    if(!this.form?.valid){
+      console.log('The form is invalid!');
+      return;
+    }
+    const user = {...this.form.value};
+    console.log(`Successful login!: ${user.email} ${user.password}`);
+    alert('Successful login!');
+  }
 }

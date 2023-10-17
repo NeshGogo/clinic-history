82311dc9ea618850828fd9cc8e853831b764f320
neshgogo <rafaelaguero10@hotmail.com>
2023-10-17 14:36:28 -0400

    auth service

--- web-app/src/app/core/services/auth.service.spec.ts
+++ web-app/src/app/core/services/auth.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 import { AuthService } from './auth.service';
+import { UserToken } from '../models/UserToken';
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -15,5 +17,20 @@ describe('AuthService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should login with email and password', (done) => {
+    var userToken: UserToken = {
+      token: 'sadsadas', 
+      expiration: new Date()
+    };
+    const loginSpy = spyOn(service, 'login').and.returnValue(of(userToken))
+    var result = service.login('test@asdas.com', 'asdsadas');
+    expect(loginSpy).toHaveBeenCalled();
+    result.subscribe(p => {
+      expect(p.token).toEqual(userToken.token);
+      expect(p.expiration).toEqual(userToken.expiration);
+      done();
+    })
   });
 });

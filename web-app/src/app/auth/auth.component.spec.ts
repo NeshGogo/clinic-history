907014038d09851244cdf71dff6616d15eee7ac0
neshgogo <rafaelaguero10@hotmail.com>
2023-10-17 14:04:31 -0400

    auth component test

--- web-app/src/app/auth/auth.component.spec.ts
+++ web-app/src/app/auth/auth.component.spec.ts
@@ -1,21 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthService } from '../core/services/auth.service';
 
 import { AuthComponent } from './auth.component';
+import { throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { By } from '@angular/platform-browser';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
+  let service: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AuthComponent]
+      imports: [AuthComponent, HttpClientTestingModule],
+      providers: [AuthService]
     });
     fixture = TestBed.createComponent(AuthComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentInstance;``
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('When component is create', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('Should build the form', () => {
+      const keys = ['email', 'password'];
+      const formkeys = Object.keys(component.form?.controls || {});
+      expect(formkeys).toEqual(keys);
+    });
+  })
+  
+  describe('When submit method is executed', () => {
+    beforeEach(() =>{
+      service = TestBed.inject(AuthService);
+    });
+
+    it('should not try to login because btn is disabled', () => {
+      const signInSpy = spyOn(service, 'login');
+      component.submit(new Event('click'));
+      expect(signInSpy).not.toHaveBeenCalled();
+    });
+
+    it('Should show an alert when login is invalid', () => {
+      const loginSpy = spyOn(service, 'login').and
+        .returnValues(throwError(() => new HttpErrorResponse({status: 400})));
+      const showAlertSpy = spyOn(component, 'showAlert');
+      component.form?.get('email')?.setValue('test@test.com');
+      component.form?.get('password')?.setValue('te21312');
+      component.submit(new Event('click'));
+      expect(loginSpy).toHaveBeenCalled();
+      expect(showAlertSpy).toHaveBeenCalled();
+    });
+
+    it('Should disable submit btn and show error if form is invalid', () => {
+      component.form?.get('email')?.setValue('test.com');
+      const btn : HTMLButtonElement = fixture.debugElement.query(By.css('button')).nativeElement;
+      expect(component.form?.get('email')?.hasError('email')).toBeTruthy();
+      expect(component.form?.get('password')?.hasError('required')).toBeTruthy();
+      expect(component.form?.invalid).toBeTruthy();
+      expect(btn.disabled).toBeTruthy();
+    });
+
+    it('Should navigate to home if authentication is success', () => {
+      
+    });
   });
 });

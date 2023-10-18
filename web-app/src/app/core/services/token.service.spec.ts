78dd5ff9cd79cbb99d7870c0ec68250c42bbaa80
neshgogo <rafaelaguero10@hotmail.com>
2023-10-18 12:38:33 -0400

    Token service test

--- web-app/src/app/core/services/token.service.spec.ts
+++ web-app/src/app/core/services/token.service.spec.ts
@@ -4,7 +4,7 @@ import { TokenService } from './token.service';
 
 describe('TokenService', () => {
   let service: TokenService;
-
+  const name = 'token'
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TokenService);
@@ -12,5 +12,34 @@ describe('TokenService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should set a token in localStorage', () => {
+    const val = 'value';
+    service.set(val);
+    expect(localStorage.getItem(name)).toEqual(val);
+  });
+
+  it('Should rewrite the token in localStorage if exists', () => {
+    const preVal = 'test'
+    localStorage.setItem(name,preVal);
+    expect(localStorage.getItem(name)).toEqual(preVal);
+    const val = 'value';
+    service.set(val);
+    expect(localStorage.getItem(name)).toEqual(val);
+  });
+
+  it('Should get the token in localStorage', () => {
+    const val = 'value';
+    localStorage.setItem(name,val);
+    const result = service.get();
+    expect(result).toEqual(val);
+  });
+
+  it('Should remove the token in localStorage', () => {
+    const val = 'value';
+    localStorage.setItem(name,val);
+    service.remove();
+    expect(localStorage.getItem(name)).toBeNull();
   });
 });

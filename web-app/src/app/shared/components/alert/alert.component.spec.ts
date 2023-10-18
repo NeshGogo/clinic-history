a0dd24a3c8673912632570876fd7ae612d03760e
neshgogo <rafaelaguero10@hotmail.com>
2023-10-18 12:55:48 -0400

    alert component test

--- web-app/src/app/shared/components/alert/alert.component.spec.ts
+++ web-app/src/app/shared/components/alert/alert.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
+import { By } from '@angular/platform-browser';
 
 describe('AlertComponent', () => {
   let component: AlertComponent;
@@ -17,5 +18,25 @@ describe('AlertComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display component with color red, button to close and content with the value test', () => {
+    component.color = 'red';
+    component.closeable = true;
+    component.content = 'test';
+    fixture.detectChanges();
+    const btn = fixture.debugElement.query(By.css('button'))
+    const element: HTMLElement = fixture.nativeElement;
+    expect(btn).toBeTruthy();
+    expect(element.innerHTML).toContain('test');
+    expect(element.innerHTML).toContain('bg-red-50');
+  });
+
+  it('should no display the button if property closable is false', () => {
+    component.color = 'red';
+    component.closeable = false;
+    component.content = 'test';
+    const btn = fixture.debugElement.query(By.css('button'))
+    expect(btn).toBeNull();
   });
 });

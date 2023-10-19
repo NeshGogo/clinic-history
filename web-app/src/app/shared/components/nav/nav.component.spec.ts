70f13fa62e7891900f4cc11c688b0c30505d7aa4
neshgogo <rafaelaguero10@hotmail.com>
2023-10-19 09:53:51 -0400

    nav component test

--- web-app/src/app/shared/components/nav/nav.component.spec.ts
+++ web-app/src/app/shared/components/nav/nav.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { NavComponent } from './nav.component';
+import { By } from '@angular/platform-browser';
 
 describe('NavComponent', () => {
   let component: NavComponent;
@@ -18,5 +19,17 @@ describe('NavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not show the user menu', () => {
+    const menu: HTMLDivElement = fixture.debugElement.query(By.css('#user-dropdown')).nativeElement;
+    expect( menu.hidden).toBeFalsy();
+  });
+
+  it('should open the user menu', () => {
+    component.ShowUserMenu(new Event('click'));
+    fixture.detectChanges();
+    const menu: HTMLDivElement = fixture.debugElement.query(By.css('#user-dropdown')).nativeElement;
+    expect( menu.classList.contains('block')).toBeTruthy();
   });
 });

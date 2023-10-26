069ba496d1e19e690b9f862993fbc14bc4e91c46
NeshGogo <rafaelaguero10@hotmail.com>
2023-10-25 20:17:15 -0400

    drawer component

---
+++ web-app/src/app/shared/components/drawer/drawer.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-drawer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './drawer.component.html',
+})
+export class DrawerComponent {
+  @Input() title: string = '';
+  @Input() direction: 'right' | 'left' = 'right';
+  @Input() open: boolean = false;
+  @Output() openChange: EventEmitter<boolean> = new EventEmitter(false);
+  isRight: boolean = this.direction == 'right';
+
+  close() {
+    this.open = false;
+    this.openChange.emit(this.open);
+  }
+}

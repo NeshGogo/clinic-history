2e63b4a4bd824cc33bd0e1ea4354ef5f21030454
NeshGogo <rafaelaguero10@hotmail.com>
2023-10-26 21:13:47 -0400

    http interceptor

--- web-app/src/app/app.config.ts
+++ web-app/src/app/app.config.ts
@@ -2,8 +2,13 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TokenInterceptor } from './core/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    { provide: HTTP_INTERCEPTORS, multi: true, useClass: TokenInterceptor },
+  ],
 };

431ee50e89e0b1d27672f86ca1e1e6fdc135a279
NeshGogo <rafaelaguero10@hotmail.com>
2023-11-01 20:35:07 -0400

    Pagination component unit test

--- web-app/src/app/shared/components/pagination/pagination.component.spec.ts
+++ web-app/src/app/shared/components/pagination/pagination.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaginationComponent } from './pagination.component';
+import { By } from '@angular/platform-browser';
 
 describe('PaginationComponent', () => {
   let component: PaginationComponent;
@@ -17,5 +18,42 @@ describe('PaginationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should go to the next page', () => {
+    component.itemsPerPage = 1;
+    component.totalItems = 5;
+    fixture.detectChanges();
+    const btn = fixture.debugElement.query(By.css('#btnNext'));
+    btn.triggerEventHandler('click');
+    expect(component.currentPage).toBe(2);
+  });
+
+  it('should not go to next page because it is last one', () => {
+    component.itemsPerPage = 2;
+    component.totalItems = 2;
+    fixture.detectChanges();
+    const btn = fixture.debugElement.query(By.css('#btnNext'));
+    btn.triggerEventHandler('click');
+    expect(component.currentPage).toBe(1);
+  });
+
+  it('should go to the previous page', () => {
+    component.itemsPerPage = 2;
+    component.totalItems = 5;
+    component.currentPage = 2
+    fixture.detectChanges();
+    const btn = fixture.debugElement.query(By.css('#btnPrev'));
+    btn.triggerEventHandler('click');
+    expect(component.currentPage).toBe(1);
+  });
+
+  it('should not go to previous page because it is first one', () => {
+    component.itemsPerPage = 2;
+    component.totalItems = 2;
+    fixture.detectChanges();
+    const btn = fixture.debugElement.query(By.css('#btnNext'));
+    btn.triggerEventHandler('click');
+    expect(component.currentPage).toBe(1);
   });
 });

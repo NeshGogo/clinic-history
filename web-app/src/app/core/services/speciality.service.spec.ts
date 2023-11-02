218d143a6e98650a9687ab121a108c832476eb12
NeshGogo <rafaelaguero10@hotmail.com>
2023-11-01 20:14:39 -0400

    Speciality service unit test

--- web-app/src/app/core/services/speciality.service.spec.ts
+++ web-app/src/app/core/services/speciality.service.spec.ts
@@ -1,7 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SpecialityService } from './speciality.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { Speciality, SpecialityCreateDto } from '../models/speciality';
+import { environment } from 'src/environments/environment';
 
 describe('SpecialityService', () => {
   let service: SpecialityService;
@@ -9,12 +14,67 @@ describe('SpecialityService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(SpecialityService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a speciality', (done) => {
+    const createDto: SpecialityCreateDto = {
+      name: 'Test Test',
+    };
+    const entity: Speciality = {
+      name: 'Test Test',
+      id: '123',
+      recordCreated: new Date(),
+      active: true,
+    };
+    service.add(createDto).subscribe((p) => {
+      expect(p).toEqual(entity);
+      done();
+    });
+    const req = httpMock.expectOne(`${environment.doctorServiceApi}/specialities`);
+    expect(req.request.method).toBe('POST');
+    req.flush(entity);
+  });
+
+  it('should get all speciality', (done) => {
+
+    const entities: Speciality[] = [
+      {
+        name: 'Test Test1',
+        id: '122313',
+        recordCreated: new Date(),
+        active: true,
+      },
+      {
+        name: 'Test Test2',
+        id: '123213',
+        recordCreated: new Date(),
+        active: true,
+      },
+      {
+        name: 'Test Test3',
+        id: '12213',
+        recordCreated: new Date(),
+        active: true,
+      },
+    ];
+    service.getAll().subscribe((p) => {
+      expect(p).toEqual(entities);
+      done();
+    });
+    const req = httpMock.expectOne(`${environment.doctorServiceApi}/specialities`);
+    expect(req.request.method).toBe('GET');
+    req.flush(entities);
   });
 });

fa6e8930122d2cbf69a01d5cfb8417c948defe40
NeshGogo <rafaelaguero10@hotmail.com>
2023-11-09 22:11:26 -0400

    updating shared components

--- web-app/src/app/shared/components/table/table.component.ts
+++ web-app/src/app/shared/components/table/table.component.ts
@@ -5,7 +5,24 @@ import { CommonModule } from '@angular/common';
   selector: 'app-table',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './table.component.html',
+  template: `
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            @for (header of headers; track $index) {
+            <th scope="col" class="px-6 py-3">
+              {{ header }}
+            </th>
+            }
+          </tr>
+        </thead>
+        <tbody>
+          <ng-content></ng-content>
+        </tbody>
+      </table>
+    </div>
+  `,
 })
 export class TableComponent {
   @Input() headers: string[] = [];

8e05569be12dd8ef610b6db10e16e946be65a88f
NeshGogo <rafaelaguero10@hotmail.com>
2023-11-22 20:58:42 -0400

    Doctor event processor at history service

---
+++ HistoryService/Dtos/DoctorPublishMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HistoryService.Dtos
+{
+    public class DoctorPublishMessageDto
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Speciality { get; set; }
+    }
+}
--- HistoryService/EventProcessing/EventProcessor.cs
+++ HistoryService/EventProcessing/EventProcessor.cs
@@ -28,6 +28,9 @@ namespace HistoryService.EventProcessing
                 case EventType.NewUser:
                     AddUser(message);
                     break;
+                case EventType.NewDoctor:
+                    AddDoctor(message);
+                    break;
                 default:
                     break;
             }
@@ -44,6 +47,9 @@ namespace HistoryService.EventProcessing
                 case EventType.NewUser:
                     _logger.LogInformation("--> New user event detected");
                     return EventType.NewUser;
+                case EventType.NewDoctor:
+                    _logger.LogInformation("--> New doctor event detected");
+                    return EventType.NewDoctor;
                 default:
                     _logger.LogInformation("--> Could not determine the event type");
                     return EventType.Undetermined;
@@ -71,6 +77,32 @@ namespace HistoryService.EventProcessing
                 catch (Exception ex)
                 {
                     _logger.LogError($"--> Could not add user to DB {ex.Message}");
+                }
+
+            }
+        }
+
+        private void AddDoctor(string doctorPublishedMessage)
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetService<IBaseRepo<Doctor>>();
+                var doctorPublishDTO = JsonSerializer.Deserialize<DoctorPublishMessageDto>(doctorPublishedMessage);
+                try
+                {
+                    var doctor = _mapper.Map<Doctor>(doctorPublishDTO);
+                    if (!repo.Exists(p => p.ExternalId == doctor.ExternalId))
+                    {
+                        repo.Add(doctor);
+                        repo.SaveChanges();
+                    }
+                    else
+                        _logger.LogWarning("--> doctor already exisits...");
+
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"--> Could not add doctor to DB {ex.Message}");
                 }
 
             }

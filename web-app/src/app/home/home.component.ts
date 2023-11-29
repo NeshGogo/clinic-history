82622578b5cf561590aa9c8a022be9fc8c3f7d4b
NeshGogo <rafaelaguero10@hotmail.com>
2023-11-28 21:35:27 -0400

    Working

--- web-app/src/app/home/home.component.ts
+++ web-app/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from '../shared/components/nav/nav.component';
 import { Title } from '@angular/platform-browser';
+import { DrawerComponent } from '../shared/components/drawer/drawer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavComponent],
+  imports: [CommonModule, NavComponent, DrawerComponent],
   template: `
     <app-nav [showOpenBtn]="false"></app-nav>
     <main class="p-2">
@@ -16,15 +17,26 @@ import { Title } from '@angular/platform-browser';
       <div class="bg-white flex flex-col justify-center items-center w-full">
         <div class="w-full max-w-3xl px-4 py-2 rounded-lg shadow-sm bg-white text-right border border-gray-100">
           <p class="font-bold text-sky-600 text-center">Are you taking on a new patient?</p>
-          <button class="px-4 py-2 rounded-lg shadow-lg text-blue-700 bg-white">Add New</button>
+          <button class="px-4 py-2 rounded-lg shadow-lg text-blue-700 bg-white" (click)="openForm()">Add New</button>
         </div>
       </div>
     </main>
+    <app-drawer
+      [title]="formTitle"
+      [(open)]="displayForm"
+    >
+
+    </app-drawer>
   `,
 })
 export class HomeComponent {
   title = 'Patients';
+  formTitle = 'Add a new patient record';
+  displayForm = false;
   constructor(titleService: Title) {
     titleService.setTitle('NC | Home');
+  }
+  openForm() {
+    this.displayForm = true;
   }
 }
